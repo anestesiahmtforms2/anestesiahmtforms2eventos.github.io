85ccbc1e485efe55b285d80016535c77b9c3f547
anestesiaforms2 <anestesiahmtforms2@gmail.com>
2026-08-07 23:01:04 -0300

    Oculta lancamento apos registrar evento

--- app.js
+++ app.js
@@ -2273,6 +2273,7 @@ async function flushQueue() {
 }
 
 function resetForm() {
+  eventEntryCard?.classList.add("hidden");
   form.reset();
   setDefaultDate();
   fillSelect(eventoSelect, currentEventoOptions, "Selecione");
